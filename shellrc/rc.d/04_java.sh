07a1040e7f59fb2879aec53e7755f66199a096f7
mmalohlava <michal@h2o.ai>
2018-07-05 00:18:30 -0700

    Enable platform settings

--- shellrc/rc.d/04_java.sh
+++ shellrc/rc.d/04_java.sh
@@ -1,28 +1,27 @@
-# Get Java home (OSX specific)
-export JAVA_10_HOME=$(/usr/libexec/java_home -v10)
-export JAVA_9_HOME=$(/usr/libexec/java_home -v9)
-export JAVA_8_HOME=$(/usr/libexec/java_home -v1.8)
-export JAVA_7_HOME=$(/usr/libexec/java_home -v1.7)
-export JAVA_6_HOME=$(/usr/libexec/java_home -v1.6)
+if [ "$PLATFORM" = "Darwin" ]; then
+    # Get Java home (OSX specific)
+    export JAVA_10_HOME=$(/usr/libexec/java_home -v10)
+    export JAVA_9_HOME=$(/usr/libexec/java_home -v9)
+    export JAVA_8_HOME=$(/usr/libexec/java_home -v1.8)
+    export JAVA_7_HOME=$(/usr/libexec/java_home -v1.7)
+    export JAVA_6_HOME=$(/usr/libexec/java_home -v1.6)
 
-# Export aliases to get proper Java
-alias java6='export JAVA_HOME=$JAVA_6_HOME'
-alias java7='export JAVA_HOME=$JAVA_7_HOME'
-alias java8='export JAVA_HOME=$JAVA_8_HOME'
-alias java9='export JAVA_HOME=$JAVA_9_HOME'
-alias java10='export JAVA_HOME=$JAVA_10_HOME'
+    # Export aliases to get proper Java
+    alias java6='export JAVA_HOME=$JAVA_6_HOME'
+    alias java7='export JAVA_HOME=$JAVA_7_HOME'
+    alias java8='export JAVA_HOME=$JAVA_8_HOME'
+    alias java9='export JAVA_HOME=$JAVA_9_HOME'
+    alias java10='export JAVA_HOME=$JAVA_10_HOME'
 
-# Default java7
-export JAVA_HOME=$JAVA_7_HOME
-#export JAVA_HOME=$JAVA_8_HOME
+    # Default java7
+    export JAVA_HOME=$JAVA_7_HOME
+
+    function javaHomes() {
+        env | grep 'JAVA_[0-9][0-9]*_HOME'
+    }
+fi
 
 function javaHome() {
   echo $JAVA_HOME
 }
-
-function javaHomes() {
-    env | grep 'JAVA_[0-9][0-9]*_HOME'
-}
-
-
 
--- shellrc/rc.d/06_aws.sh
+++ shellrc/rc.d/06_aws.sh
@@ -2,13 +2,13 @@
 export EC2_HOME="$HOME/.ec2"
 export EC2_PRIVATE_KEY=$EC2_HOME/keys/0xdata_Big.pem
 #export EC2_CERT=~/.ec2/certs/cert-BVX272T5VKWSEMA7DVBT2C2TAELRKOW7.pem
-export AWS_ACCESS_KEY_ID=$(head $EC2_HOME/aws_id)
-export AWS_SECRET_ACCESS_KEY=$(head $EC2_HOME/aws_key)
+export AWS_ACCESS_KEY_ID=$(test -f $EC2_HOME/aws_id && head $EC2_HOME/aws_id)
+export AWS_SECRET_ACCESS_KEY=$(test -f $EC2_HOME/aws_key && head $EC2_HOME/aws_key)
 export AWS_ACCESS_KEY=$AWS_ACCESS_KEY_ID
 export AWS_SECRET_KEY=$AWS_SECRET_ACCESS_KEY
 export AWS_CREDENTIAL_FILE="$EC2_HOME/0xdataAwsCredentials.properties"
 
-if [ "$(uname -s)" == "Darwin" ]; then
+if [ "$(uname -s)" = "Darwin" ]; then
     export AWS_CLOUDFORMATION_HOME="$(brew --prefix)/Cellar/aws-cfn-tools/1.0.12/libexec"
     export EC2_HOME="$(brew --prefix)/Cellar/ec2-api-tools/1.7.1.0/libexec"
 fi
