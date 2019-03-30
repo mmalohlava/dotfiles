af4f0b08a5ba6dcbc09f80f49e5d88335b5549cc
mmalohlava <michal@h2o.ai>
2019-03-29 22:34:50 -0700

    Fixing command execution
    Based on: https://stackoverflow.com/a/32280085

--- shellrc/rc.d/07_dai.sh
+++ shellrc/rc.d/07_dai.sh
@@ -4,9 +4,9 @@ function runDai() {
 
     id=$1
     shift
-    cmd="nvidia-docker run $@ -e DRIVERLESS_AI_DEBUG_LOG=1 -v /home/michal/dai/license/:/license -e DRIVERLESS_AI_AWS_AUTH='False' -e DRIVERLESS_AI_ENABLED_HDFS_AUTH_TYPE='noauth'  -p 12333:12345 --init -it --rm -v /tmp/dtmp/:/tmp -v /tmp/dlog/:/log -u $(id -u):$(id -g)"
-    echo "$cmd $id"
-    $cmd "$id"
+    cmd=( nvidia-docker run $@ -e DRIVERLESS_AI_DEBUG_LOG=1 -v /home/michal/dai/license/:/license -e DRIVERLESS_AI_AWS_AUTH='False' -e DRIVERLESS_AI_ENABLED_HDFS_AUTH_TYPE='noauth'  -p 12333:12345 --init -it --rm -v /tmp/dtmp/:/tmp -v /tmp/dlog/:/log -u $(id -u):$(id -g) )
+    echo "> ${cmd[@]} $id"
+    "${cmd[@]}" "$id"
 }
 
 function runDaiShell() {
