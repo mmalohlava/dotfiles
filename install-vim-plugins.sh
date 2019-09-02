73d7ef50cf0faf18553e0011a76584df09aa8a18
mmalohlava <michal@h2o.ai>
2019-09-02 16:29:43 -0700

    Added coc.nvim plugin

--- install-vim-plugins.sh
+++ install-vim-plugins.sh
@@ -3,16 +3,26 @@
 VIM_PLUGINS="https://github.com/NLKNguyen/papercolor-theme \
     https://github.com/nathanaelkane/vim-indent-guides \
     https://github.com/mhinz/vim-startify \
-    https://github.com/cohama/agit.vim"
+    https://github.com/cohama/agit.vim \
+    https://github.com/neoclide/coc.nvim.git@release"
 
 BASE_DIR=${HOME}/.vim_runtime/my_plugins
+[ -d "${BASE_DIR}" ] || mkdir -p "${BASE_DIR}"
 
 for PLUGIN in $VIM_PLUGINS; do
-    dirname=${BASE_DIR}/$(basename "${PLUGIN}")
+    plugin=$PLUGIN
+    branch=master
+    if [[ $PLUGIN == *"@"* ]]; then
+        plugin=$(echo "$PLUGIN" | cut -d@ -f1)
+        branch=$(echo "$PLUGIN" | cut -d@ -f2)
+    fi
+    dirname=${BASE_DIR}/$(basename "${plugin}")
     if [ -d "${dirname}" ]; then
+        echo "* Pull ${plugin}"
         ( cd "${dirname}" && git pull )
     else
-        ( cd "${BASE_DIR}" && git clone "${PLUGIN}")
+        echo "* Clone ${plugin} (branch: ${branch})"
+        ( cd "${BASE_DIR}" && git clone --branch "${branch}" "${plugin}")
     fi
 done
 
