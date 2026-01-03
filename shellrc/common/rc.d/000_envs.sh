f63a51ae1e58f5c8fbbb41d0fa4d8ac4cffdc1be
Michal Malohlava <michal.malohlava@gmail.com>
2026-01-02 21:23:06 -0800

    fix: number of fixes for github

--- shellrc/common/rc.d/000_envs.sh
+++ shellrc/common/rc.d/000_envs.sh
@@ -10,7 +10,13 @@ else
 fi
 
 # Export PATH
-export PATH=$PATH:$BIN_DIR
+export PATH=$PATH:$BIN_DIR:$HOME/.local/bin
+if [ -n "${BREW_DIR}" ]; then
+    export PATH=$PATH:${BREW_DIR}/opt/coreutils/libexec/gnubin
+    export PATH=${BREW_DIR}/sbin:$PATH
+    export PATH=${BREW_DIR}/bin:$PATH
+    export MANPATH="${BREW_DIR}/opt/coreutils/libexec/gnuman:$MANPATH"
+fi
 
 # favorite editor
 export VISUAL=nvim
--- shellrc/common/rc.d/01_aliases.sh
+++ shellrc/common/rc.d/01_aliases.sh
@@ -25,6 +25,8 @@ alias doc="cd $HOME/Documents"
 alias dbox="cd $HOME/Dropbox/"
 alias dload="cd $HOME/Downloads/"
 alias acroread="open -a 'Adobe Acrobat Reader DC'"
+alias vim=nvim
+alias vi=nvim
 
 # Needs: https://github.com/sharkdp/bat
 if which bat >/dev/null; then
@@ -41,29 +43,6 @@ dfu() {
 # Jump to directory containing file
 jump() {
     cd "$(dirname ${1})"
-}
-
-
-up()
-{
-    local cdir="$(pwd)"
-    if [[ "${1}" == "" ]]; then
-        cdir="$(dirname "${cdir}")"
-    elif ! [[ "${1}" =~ ^[0-9]+$ ]]; then
-        echo "Error: argument must be a number"
-    elif ! [[ "${1}" -gt "0" ]]; then
-        echo "Error: argument must be positive"
-    else
-        for ((i=0; i<${1}; i++)); do
-            local ncdir="$(dirname "${cdir}")"
-            if [[ "${cdir}" == "${ncdir}" ]]; then
-                break
-            else
-                cdir="${ncdir}"
-            fi
-        done
-    fi
-    cd "${cdir}"
 }
 
 function ppgrep() {
